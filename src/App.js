532374ad72fe5cc5f8cb9719378f189626fbee05
Mateusz Kibiłko <mateusz.kibilko@gmail.com>
2020-06-01 14:38:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import {
+    Table,
+    TableBody,
+    TableRow,
+    TableHead,
+    TableCell,
+} from "@material-ui/core";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function generateAlphabet() {
+        return [...Array(26)].map((_, y) => String.fromCharCode(y + 65));
+    }
+
+    let alphabet = generateAlphabet();
+
+    useEffect(() => {
+        let people;
+        axios
+            .get(
+                "https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people"
+            )
+            .then((res) => {
+                people = res.data.results;
+                let count = res.data.count;
+                let promises = [];
+                let pages = Math.ceil(count / 10);
+
+                for (let i = 2; i <= pages; i++) {
+                    promises.push(
+                        axios.get(
+                            `https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people?page=${i}`
+                        )
+                    );
+                }
+
+                return Promise.all(promises);
+            })
+            .then((response) => {
+                let results = response.map((el) => el.data.results);
+                people = people.concat(...results);
+                console.log(people);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+
+    return (
+        <div className="App">
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Eyes color</TableCell>
+                        <TableCell>Gender</TableCell>
+                        <TableCell>Hair Color</TableCell>
+                        <TableCell>Height</TableCell>
+                        <TableCell>Mass</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {alphabet.map((el, index) => (
+                        <TableRow key={index}>
+                            <TableCell colSpan="6">{el}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
 }
 
 export default App;
